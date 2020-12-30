9698f87aa35cd447232df321cd71714989cc8538
MrMilul <miladmizani@yahoo.com>
2020-12-30 17:09:20 +0200

    a bit improvement

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,17 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './main.css'
+import CalculateTimer from '../../helper/CalculateTimer'
+import Controler from '../controles/control'
 
 function Main(){
     const [timeInSecond, setTimeInSecond] = useState<number>(0)
     
+    const [timerArray, setTimerArray] = useState<Array<number | string>>([])
+
+    useEffect(()=>{
+        let timeArray: Array<number|string> = CalculateTimer(timeInSecond)
+        setTimerArray(timeArray)
+    } , [timeInSecond])
     return(
-        <section className="time-container">
-            <p className='timer-text'>0</p>
-            <span>:</span>
-            <p className='timer-text'>0</p>
-            <span>:</span>
-            <p className='timer-text'>0</p>   
-        </section>
+        <>
+            <section className="time-container">
+                <p className='timer-text'>{timerArray[0]}</p>
+                <span>:</span>
+                <p className='timer-text'>{timerArray[1]}</p>
+                <span>:</span>
+                <p className='timer-text'>{timerArray[2]}</p>       
+            </section>
+             < Controler setTimeInSecond = {setTimeInSecond}/>
+        </>
     )
 }
 
