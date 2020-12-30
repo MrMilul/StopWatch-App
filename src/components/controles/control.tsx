9c91b8e6319eaa0f7422fa14d64079c68afe6b30
MrMilul <miladmizani@yahoo.com>
2020-12-30 17:07:38 +0200

    Play, Stop, and Reset button

--- src/components/controles/control.tsx
+++ src/components/controles/control.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './control.css'
 
-function Control(){
+
+type Props = {
+    setTimeInSecond: Function 
+}
+
+
+function Controler(props:Props){
+    const { setTimeInSecond } = props;
+    const [intervalId, setIntervalId] = useState<number>(0)
+
+    const handlePlayBut = () =>{
+            let interval:any = setInterval(()=>{
+                setTimeInSecond((previousState:number)=> 
+                    previousState + 1)
+            }, 1000)
+        setIntervalId(interval)
+    }
+
+    const handleStopBut = () =>{
+        clearInterval(intervalId)   
+    }
+
+    const handleResetBut = () => {
+        clearInterval(intervalId)
+        setTimeInSecond(0)
+
+    }
     return(
-        <section>
-
+        <section className="controles-container">
+            <button onClick={handlePlayBut}>Play</button>
+            <button onClick={handleStopBut}>Stop</button>
+            <button onClick={handleResetBut}>Reset</button>
         </section>
     )
 }
 
-export default Control
+export default Controler
