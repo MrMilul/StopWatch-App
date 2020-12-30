1a2decb78fc33cadf42e0c0115df85ecd5f8711c
MrMilul <miladmizani@yahoo.com>
2020-12-30 17:06:47 +0200

    it is the engine!

--- src/helper/CalculateTimer.ts
+++ src/helper/CalculateTimer.ts
@@ -1,13 +1,16 @@
 
-function CalculateTimer(timeInsec:number):Array<number> {
+function CalculateTimer(timeInsec:number):Array<number | string> {
     let hours:number = Math.floor(timeInsec/3600);
     let minutes:number = Math.floor((timeInsec - (hours * 3600)) / 60);
     let seconds: number = timeInsec - (hours * 3600) - (minutes * 60);
 
+    let hourFormat = hours < 10 ? `0${hours}` : hours;
+    let minutesFormat = minutes < 10 ? `0${minutes}` : minutes;
+    let secondsFormat = seconds < 10 ? `0${seconds}` : seconds;
     return [
-        hours,
-        minutes,
-        seconds
+        hourFormat,
+        minutesFormat,
+        secondsFormat
     ]
 }
 
